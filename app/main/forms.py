63acd4c4c6b80692506ab6b0a21ca4f873a26682
HannahChege <chegehannah45@gmail.com>
2018-09-10 19:29:47 +0300

    Adde chanages to the index.html

---
+++ app/main/forms.py
@@ -0,0 +1,22 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField,TextAreaField,SubmitField,BooleanField
+from wtforms.validators import Required
+
+class ContentForm(FlaskForm):
+   content = TextAreaField('YOUR PITCH')
+   submit = SubmitField('SUBMIT')
+
+class CommentForm(FlaskForm):
+   comment_id = TextAreaField('WRITE COMMENT')
+   submit = SubmitField('SUBMIT')
+
+class PitchForm(FlaskForm):  # create a class that inherits from FlaskForm class
+   name = StringField('Authors Name', validators=[Required()])
+   categoy = TextAreaField('Pitch', validators=[Required()])
+   pitch = TextAreaField('Pitch', validators=[Required()])
+   submit = SubmitField('Submit')
+
+
+class UpdateProfile(FlaskForm):
+   bio = TextAreaField('Tell us about you.', validators=[Required()])
+   submit = SubmitField('Submit')
