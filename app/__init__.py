652f52459adb06abf8677020ab1008df271b06aa
HannahChege <chegehannah45@gmail.com>
2018-09-11 12:55:37 +0300

    pip freezed

--- app/__init__.py
+++ app/__init__.py
@@ -4,11 +4,12 @@ from flask_bootstrap import Bootstrap
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 
-bootstrap = Bootstrap()
-db = SQLAlchemy()
+
 login_manager = LoginManager()
 login_manager.session_protection = 'strong'
 login_manager.login_view = 'auth.login'
+bootstrap = Bootstrap()
+db = SQLAlchemy()
 
 def create_app(config_name):
  
