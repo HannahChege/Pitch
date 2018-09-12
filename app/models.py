f072a5786403fd9891c56ab1c9aaedd0c1335b9f
HannahChege <chegehannah45@gmail.com>
2018-09-12 14:32:35 +0300

    Added bootstrap

--- app/models.py
+++ app/models.py
@@ -1,9 +1,11 @@
-from . import db
+from . import db,login_manager
 from werkzeug.security import generate_password_hash,check_password_hash
 from flask_login import UserMixin
 
 #...
-
+@login_manager.user_loader
+def load_user(user_id):
+    return None
 class User(UserMixin,db.Model):
     __tablename__ = 'users'
     id = db.Column(db.Integer,primary_key = True)
---
+++ start.sh
@@ -0,0 +1,6 @@
+export SECRET_KEY='<Flask WTF Secret Key>'
+
+export MAIL_USERNAME='chegehannah45@gmail.com'
+export MAIL_PASSWORD='scarletshevannah'
+
+python manage.py server
