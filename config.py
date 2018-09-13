462a78f3af6af4b9739d544ba0d232b1ab0eb73e
HannahChege <chegehannah45@gmail.com>
2018-09-13 15:21:06 +0300

    Added changes

--- config.py
+++ config.py
@@ -1,24 +1,23 @@
 import os
 
 class Config:
-    SECRET_KEY = 'scarletshevannah'
     UPLOADED_PHOTOS_DEST ='app/static/photos'
-    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://:hannah@localhost/pitch'
+    
 
-
-
-#  email configurations
+    @staticmethod
+    def init_app(app):
+        pass
+class ProdConfig(Config):
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
+    SECRET_KEY = os.environ.get('SECRET_KEY')
+    
+    #  email configurations
     MAIL_SERVER = 'smtp.googlemail.com'
     MAIL_PORT = 587
     MAIL_USE_TLS = True
     MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
     MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
     
-    @staticmethod
-    def init_app(app):
-        pass
-class ProdConfig(Config):
-    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
 
 class TestConfig(Config):
     SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://hannah:hannah@localhost/pitch_test'
--- start.sh
+++ start.sh
@@ -1,3 +1,6 @@
-export MAIL_USERNAME=chegehannah45@gmail.com
-export MAIL_PASSWORD=scarletshevannah
+export MAIL_USERNAME='chegehannah45@gmail.com'
+export MAIL_PASSWORD='scarletshevannah'
+export SECRET_KEY = 'scarletshevannah'
+export DATABASE_URL = 'postgresql+psycopg2://:hannah@localhost/pitch'
+
 python3.6 manage.py server
